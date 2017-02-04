88a5da91c7d20f783b90d10f5ed7b138c529694b
robert-laws <robaws@me.com>
2017-02-04 18:38:15 +0300

    refactored to ES6 syntax

--- app/components/About.js
+++ app/components/About.js
@@ -1,14 +1,14 @@
 var React = require('react');
-var { Container } = require('semantic-ui-react');
+var { Container, Segment } = require('semantic-ui-react');
 
-var About = React.createClass({
-    render() {
-        return (
-            <Container id="topDiv">
-                <h1>About Me and this Website.</h1>
-            </Container>
-        )
-    }
-});
+var About = (props) => {
+  return (
+    <Container id="topDiv">
+      <Segment>
+        <h1>About Me and this Website...</h1>
+      </Segment>
+    </Container>
+  )
+}
 
 module.exports = About;
