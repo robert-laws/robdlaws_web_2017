11ae104e802cf433179f675334f73f5ff1a021a3
Robert Laws <roblaws@me.com>
2017-03-05 08:35:41 +0300

    fix social networking mixup commit

---
+++ app/components/ResourceCard.js
@@ -0,0 +1,26 @@
+var React = require('react');
+var { Card, Image } = require('semantic-ui-react');
+
+var ResourceCard = React.createClass({
+  render: function() {
+    var { title, subTitle, desc, link, imgSrc } = this.props;
+    return (
+      <Card>
+        <Image src={'images/' + imgSrc} />
+        <Card.Content>
+          <Card.Header>
+            <a href={link} target='_blank'>{title}</a>
+          </Card.Header>
+          <Card.Meta>
+            <span className='date'>
+              {subTitle}
+            </span>
+          </Card.Meta>
+          <Card.Description>
+            {desc}
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    )
+  }
+});
--- app/components/SocialNetworking.js
+++ app/components/SocialNetworking.js
@@ -1,49 +1,20 @@
-// presentational
-
 var React = require('react');
-<<<<<<< HEAD
 var { Container, Header, Divider } = require('semantic-ui-react');
-var ResourceCard = require('ResourceCard');
+var ResourceCards = require('ResourceCards');
+
+import { cards } from './../data/cards/data.json';
 
 var SocialNetworking = React.createClass({
-  getInitialState: function() {
-    return {
-      items: []
-    }
-  },
-  componentDidMount: function() {
-    var {cards} = this.props;
-    var getCards = cards.map(function(card) {
-      return <ResourceCard key={card.id} title={card.title} subTitle={card.subTitle} desc={card.desc} link={card.link} imgSrc={card.imgSrc} />;
-    });
-
-    this.setState({
-      items: getCards
-    });
-  },
   render() {
-    var {cards} = this.props;
     return (
       <Container id="topDiv">
         <Header as="h1">Social Networking</Header>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <Divider />
-        {this.state.items}
+        <ResourceCards resourceCards={cards} />
       </Container>
     )
   }
-=======
-var { Container, Header } = require('semantic-ui-react');
-
-var SocialNetworking = React.createClass({
-    render() {
-        return (
-            <Container id="topDiv">
-                <Header as="h1">Social Networking</Header>
-            </Container>
-        )
-    }
->>>>>>> parent of aef9faa... begin preparing cards - social networking
 });
 
 module.exports = SocialNetworking;
